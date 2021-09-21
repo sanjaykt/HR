d0fccaf977faecdb4c0a1e41339bacc9b3a76f0c
Sanjay Tailor <st.sanjaytailor@gmail.com>
2021-09-21 11:12:46 +0530

    add database 'hr'

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./users/user.entity";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'ma00amma',
+      database: 'hr',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+
+export class AppModule {
+}
